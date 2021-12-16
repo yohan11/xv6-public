a049b00763b86f26fcda16ca90d7cdcddfb82339
yohan11 <yipco77@naver.com>
2021-12-16 23:14:59 +0900

    sln 시스템콜 구현

---
+++ sln.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "stddef.h"
+
+
+int main(int argc, char* argv[])
+{
+
+if(argc != 3){
+    printf(2, "Usage: ln old new\n");
+    exit();
+  }
+  if(link(argv[1], argv[2]) < 0)
+    printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+  exit();
+}
